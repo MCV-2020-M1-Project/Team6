aedb019773ab9382bf58d5cf766841827ddfb654
VanillaTiger <a.szummer@gmail.com>
2020-10-08 18:34:10 +0200

    cleaning formating

--- mask_evaluation.py
+++ mask_evaluation.py
@@ -1,39 +1,39 @@
 import cv2 as cv
 import numpy as np
 
+
 def mask_evaluation(annotation, result):
-    #result=cv.imread("mask-dilate.png",0)
-    #annotation=cv.imread(r"C:\Users\adama\OneDrive\Dokumenty\Master of Computer Vision\M1\week 1\qsd2_w1\00004.png",0)
+    #result=cv.imread(r"sandbox\\mask-dilate.png",0)
+    #annotation=cv.imread(r"qsd2_w1\\00004.png",0)
 
-    result_neg=cv.bitwise_not(result)
-    annotation_neg=cv.bitwise_not(annotation)
+    result_neg = cv.bitwise_not(result)
+    annotation_neg = cv.bitwise_not(annotation)
 
-    TP = annotation & result
-    FN = annotation & result_neg
-    FP = annotation_neg & result
-    TN = annotation_neg & result_neg
+    tp = annotation & result
+    fn = annotation & result_neg
+    fp = annotation_neg & result
+    tn = annotation_neg & result_neg
 
-    cv.imshow('TP',TP)
-    cv.imshow('FN',FN)
-    cv.imshow('FP',FP)
-    cv.imshow('TN',TN)
+    cv.imshow('TP', tp)
+    cv.imshow('FN', fn)
+    cv.imshow('FP', fp)
+    cv.imshow('TN', tn)
 
     cv.waitKey()
 
-    TP=TP.sum()
-    FN=FN.sum()
-    FP=FP.sum()
-    TN=TN.sum()
+    tp = tp.sum()
+    fn = fn.sum()
+    fp = fp.sum()
+    tn = tn.sum()
 
-    print(TP,FN,FP,TN)
+    print(tp, fn, fp, tn)
 
-    precision=TP/(TP+FP)
-    recall=TP/(TP+FN)
-    f1_measure=2*(precision*recall/(precision+recall))
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1_measure = 2 * (precision * recall / (precision + recall))
 
-    print("Precision=",precision)
-    print("Recall=",recall)
-    print("F1 measure=",f1_measure)
+    print("Precision=", precision)
+    print("Recall=", recall)
+    print("F1 measure=", f1_measure)
 
-    return (precision,recall,f1_measure)
-
+    return precision, recall, f1_measure
