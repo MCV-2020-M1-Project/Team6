f3cd6c5479e7c4da0781eeb0a94fd19f8c7c1118
Carmen García Nogueiras <carmengnogu@gmail.com>
2020-10-09 14:54:23 +0200

    Descriptors obtention methods added to descriptor_lib.py (color space hist concatenated) and minor corrections to index_db.py

--- index_db.py
+++ index_db.py
@@ -1,12 +1,14 @@
 import pickle
-import descriptor_lib as desc
 import cv2
+import descriptor_lib as desc
 
-#generate descriptors for all images in the BBDD
-img_list = [cv2.imread('../BBDD/bbdd_00'+ '{:03d}'.format(i) +'.jpg', cv2.IMREAD_GRAYSCALE)\
-    for i in range(287)]
-descriptors_dic = desc.get_descriptors(img_list)
+#Generate descriptors for all images in the BBDD
+descript_dic_list = []
 
-#save descriptors
+for i in range(287):
+    img = cv2.imread('../BBDD/bbdd_00'+ '{:03d}'.format(i) +'.jpg', cv2.IMREAD_COLOR)
+    descript_dic_list.append(desc.get_descriptors(img)) #get a dic with the descriptors for the img
+
+#Save descriptors
 with open('bd_descriptors.pkl', 'wb') as dbfile:
-    pickle.dump(descriptors_dic, dbfile)
+    pickle.dump(descript_dic_list, dbfile)
