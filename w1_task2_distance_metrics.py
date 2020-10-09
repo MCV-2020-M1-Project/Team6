897c3f05463cbf3d57f5144e470eb6494a54f668
jchavescoelho <jchavescoelho@gmail.com>
2020-10-09 02:37:47 +0200

    Added .gitignore. Added pip requirements. Added visual display of compared histograms and measures

--- w1_task2_distance_metrics.py
+++ w1_task2_distance_metrics.py
@@ -13,6 +13,8 @@ Metrics implemented:
 '''
 import random as rnd
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 
 def get_euclidean_distance(descriptor_a, descriptor_b):
@@ -46,7 +48,7 @@ def get_hist_intersection(descriptor_a, descriptor_b):
 
 
 def get_hellinger_kernel(descriptor_a, descriptor_b):
-     '''
+    '''
     Gets descriptors as numpy arrays and returns hellinger kernel (whatever that is)
     '''
     if any(descriptor_a < 0) or any(descriptor_b < 0):
@@ -54,11 +56,69 @@ def get_hellinger_kernel(descriptor_a, descriptor_b):
         return -1
     return np.sum(np.sqrt(descriptor_a*descriptor_b))
 
+
+def display_comparison(a, b):
+    '''
+    Displays an iamge with both descriptors (as histograms) alongside calculated measures
+
+    This is kind of a silly thing, more showy than anything, but it might be useful when triying to
+    decide which distance works best
+    '''
+    # image
+    display_m_img = np.zeros((460, 828, 3), dtype=np.uint8)
+
+    # measures
+    text = ['Euclidean: ' + str(round(get_euclidean_distance(a, b), 2)),
+            'X2: ' + str(round(get_x2_distance(a, b), 2)),
+            'L1: ' + str(round(get_l1_distance(a, b), 2)), 
+            'Hist intersection: ' + str(round(get_hist_intersection(a, b), 2)),
+            'Hellinger Kernel: ' + str(round(get_hellinger_kernel(a, b), 2))
+        ]
+
+    # Draw histograms
+    ## Some position parameters
+    hist_sq_size = (512, 200)
+
+    x_offset = 20
+    bt_y_hist_1 = 220
+    bt_y_hist_2 = 440
+
+    measure_text_pos = (552, 20)
+
+    ## Draw first hist
+    for k, v in enumerate(a):
+        cv2.line(display_m_img, (int(hist_sq_size[0]*k/len(a)) + x_offset, bt_y_hist_1),
+                                (int(hist_sq_size[0]*k/len(a)) + x_offset, bt_y_hist_1 - int(hist_sq_size[1]*v/100)),
+                                (0, 255, 0)
+                                )
+
+    ## Draw second hist
+    for k, v in enumerate(b):
+        cv2.line(display_m_img, (int(hist_sq_size[0]*k/len(b)) + x_offset, bt_y_hist_2), 
+                                (int(hist_sq_size[0]*k/len(b)) + x_offset, bt_y_hist_2 - int(hist_sq_size[1]*v/100)),
+                                (0, 0, 255)
+                                )
+
+    ## Display text
+    y = measure_text_pos[1]
+    for t in text:
+        cv2.putText(display_m_img, t, (measure_text_pos[0], y), cv2.FONT_HERSHEY_COMPLEX, .5, (255, 0, 0))
+        y += 15
+
+
+    cv2.imshow('Display', display_m_img)
+    cv2.waitKey(0)
+
+    return
+
+
 # at least it runs?
 a = np.array([rnd.uniform(0, 100) for i in range(255)])
 b = np.array([rnd.uniform(0, 100) for i in range(255)])
 
-print('Euclidean:', get_euclidean_distance(a, a))
+display_comparison(a, b)
+
+print('Euclidean:', get_euclidean_distance(a, b))
 print('L1:', get_l1_distance(a, b))
 print('X2:', get_x2_distance(a, b))
 print('Histogram intersection:', get_hist_intersection(a, b))
