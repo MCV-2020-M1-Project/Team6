946c8fb43c1dd8443b1a52c6c1beddf88d26b305
VanillaTiger <a.szummer@gmail.com>
2020-10-10 22:38:21 +0200

    generate report method

--- mask_segmentation.py
+++ mask_segmentation.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
+
 def show_xyz(im, colorspace):
     """
     im - number from the name of the file
@@ -21,13 +22,16 @@ def show_xyz(im, colorspace):
     if colorspace == 'hsv': im = cv.cvtColor(im, cv.COLOR_BGR2HSV, im)
     if colorspace == 'ycrcb': im = cv.cvtColor(im, cv.COLOR_BGR2YCrCb, im)
     if colorspace == 'cielab': im = cv.cvtColor(im, cv.COLOR_BGR2Lab, im)
+    if colorspace == 'xyz': im = cv.cvtColor(im, cv.COLOR_BGR2XYZ, im)
+    if colorspace == 'yuv': im = cv.cvtColor(im, cv.COLOR_BGR2YUV, im)
 
     if colorspace != "gray":
         x, y, z = cv.split(im)
         cv.imshow('x', x)
         cv.imshow('y', y)
         cv.imshow('z', z)
-    else: cv.imshow('gray',im)
+    else:
+        cv.imshow('gray', im)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -54,6 +58,8 @@ def generate_masks(x_range, y_range, z_range, colorspace):
         if colorspace == 'hsv': im = cv.cvtColor(im, cv.COLOR_BGR2HSV, im)
         if colorspace == 'ycrcb': im = cv.cvtColor(im, cv.COLOR_BGR2YCrCb, im)
         if colorspace == 'cielab': im = cv.cvtColor(im, cv.COLOR_BGR2Lab, im)
+        if colorspace == 'xyz': im = cv.cvtColor(im, cv.COLOR_BGR2XYZ, im)
+        if colorspace == 'yuv': im = cv.cvtColor(im, cv.COLOR_BGR2YUV, im)
 
         im_annotation = cv.imread(obraz, 0)
         # mask color
@@ -67,7 +73,7 @@ def generate_masks(x_range, y_range, z_range, colorspace):
         if not os.path.exists('../datasets/masks_extracted/'):
             os.makedirs('../datasets/masks_extracted/')
 
-        cv.imwrite('../datasets/masks_extracted/' + obraz[-8:], mask0)
+        #cv.imwrite('../datasets/masks_extracted/' + obraz[-9:], mask0)
 
         p, r, f = mask_evaluation.mask_evaluation(im_annotation, mask0)
 
@@ -82,14 +88,15 @@ def generate_masks(x_range, y_range, z_range, colorspace):
     return masks_measures
 
 
-def generate_measures_output(measures_data, show_graph=False):
+def generate_measures_output(input_measures_data, show_graph=False):
     """
     show_graph True if you want to see scatterplot of measures
     measure data as list of lists
 
     """
-    df = pd.DataFrame(np.array(measures_data), columns=['image', 'precision', 'recall', 'f1'])
+    df = pd.DataFrame(np.array(input_measures_data), columns=['image', 'precision', 'recall', 'f1'])
     df.head()
+    df.to_excel("../datasets/masks_extracted/output_measures.xlsx")
 
     sns.relplot(data=df,
                 y='image',
@@ -106,15 +113,15 @@ def generate_measures_output(measures_data, show_graph=False):
                 x='f1',
                 aspect=2.5).set_titles('f1')
 
-    tresh = 0.8
+    thresh = 0.8
 
-    print("Treshold = ", tresh)
+    print("Treshold = ", thresh)
     print("----------------------ABOVE TRESHOLD--------------------")
-    print(df.f1[df['f1'] > tresh].count())
+    print(df.f1[df['f1'] > thresh].count())
     print("-----------------------BELOW TRESHOLD--------------------")
-    print(df.f1[df['f1'] < tresh].count())
+    print(df.f1[df['f1'] < thresh].count())
     print("----------------------LIST ABOVE TRESHOLD--------------------")
-    print(df[df['f1'] > tresh])
+    print(df[df['f1'] > thresh])
     print("-------------AVERAGE PRECISION------------------------")
     print(df['precision'].mean())
     print("-------------AVERAGE RECALL------------------------")
@@ -125,15 +132,67 @@ def generate_measures_output(measures_data, show_graph=False):
     if show_graph: plt.show()
 
 
+def balance_white_image(im):
+    image = cv.imread(f'../datasets/qsd2_w1/{im}.jpg')
+
+    wb = cv.xphoto.createGrayworldWB()
+    wb.setSaturationThreshold(0.99)
+    image = wb.balanceWhite(image)
+
+    return image
+
+
+def equalize_histogram(im):
+    im = cv.imread(f'../datasets/qsd2_w1/{im}.jpg')
+    im = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    dst = cv.equalizeHist(im)
+
+    return dst
+
+
 # img = cv.imread('../datasets/qsd2_w1/00004.jpg')
 # img_annotation = cv.imread('../datasets/qsd2_w1/00004.png',0)
 
-#show_xyz('00004','gray')
+# show_xyz('00004','yuv')
 
-# measures_data = generate_masks([0,30],[120,255],[50,255], 'hsv')
-# measures_data = generate_masks([50,255],[120,255],[50,255], 'bgr')
-# measures_data = generate_masks([0,255],[120,255],[120,255], 'cielab')
-measures_data = generate_masks([50,255],[120,255],[50,255], 'ycrcb')
+# measures_data = generate_masks([0,20],[150,255],[0,255], 'hsv')
+#measures_data = generate_masks([50,255],[120,255],[50,255], 'bgr')
+# measures_data = generate_masks([100,255],[0,255],[0,255], 'cielab')
+#measures_data = generate_masks([50, 255], [0, 255], [100, 255], 'ycrcb')
 
-generate_measures_output(measures_data, True)
+#generate_measures_output(measures_data, False)
 
+# cv.imshow('eqlz',equalize_histogram('00024'))
+# cv.waitKey()
+# show_xyz('00017','ycrcb')
+# cv.waitKey()
+
+"""
+## what if we just take histogram of center of the photo where for sure is
+## and compare against other centers
+
+#ycrb works great for picture 24 and 10 - why ?
+#blue cut out at 108-134 is best with red and green equal to 0 from all channels
+
+how well it will find cut out photos with 0,79 F1 ?
+what if we take hue channel 0-20 where precision in 86% and low recall and connect with blue channel over 150
+which has high recall over 90%
+
+"""
+
+def generate_report():
+
+    all_mean_measures=[]
+
+    for i in range(0,255):
+        input_measures_data = generate_masks([0,255],[0,255],[i,255],'hsv')
+        df = pd.DataFrame(np.array(input_measures_data), columns=['image', 'precision', 'recall', 'f1'])
+
+        mean_measures = [i, df['precision'].mean(),df['recall'].mean(), df['f1'].mean()]
+        all_mean_measures.append((mean_measures))
+        print(i)
+
+    df_all = pd.DataFrame(np.array(all_mean_measures), columns=['value value', 'precision', 'recall', 'f1'])
+    df_all.to_excel("../output_measures_hsv_v.xlsx")
+
+generate_report()
