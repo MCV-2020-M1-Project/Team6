5b17bc0ee6c5caeb5c5af78eca661ae3fff53c58
Carmen García Nogueiras <carmengnogu@gmail.com>
2020-10-14 21:14:35 +0200

    Added hs_multi_hist and some error fixed

--- descriptor_lib.py
+++ descriptor_lib.py
@@ -123,23 +123,44 @@ def get_hsv_concat_hist_blur(img):
     hist_v = cv2.calcHist([hsv],[2],None,[256],[0,256])
     return np.concatenate((hist_h, hist_s, hist_v))
 
-def get_multi_hist(img):
+def get_h_multi_hist(img):
     ''' Paramenters: img (color image)
         Returns: return several hist concat result of splitting the image '''
 
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     (h, w) = hsv.shape[:2]
-    tile1 = hsv[0:h/2, 0:w/2]
-    tile2 = hsv[0:h/2, w/2:-1]
-    tile3 = hsv[h/2:-1, 0:w/2]
-    tile4 = hsv[h/2:-1, w/2:-1]
+    tile1 = hsv[0:h//2, 0:w//2]
+    tile2 = hsv[0:h//2, w//2:-1]
+    tile3 = hsv[h//2:-1, 0:w//2]
+    tile4 = hsv[h//2:-1, w//2:-1]
 
     hist_tile1 = cv2.calcHist([tile1],[0],None,[256],[0,256])
-    hist_tile2 = cv2.calcHist([tile2],[1],None,[256],[0,256])
-    hist_tile3 = cv2.calcHist([tile3],[2],None,[256],[0,256])
-    hist_tile4 = cv2.calcHist([tile4],[2],None,[256],[0,256])
+    hist_tile2 = cv2.calcHist([tile2],[0],None,[256],[0,256])
+    hist_tile3 = cv2.calcHist([tile3],[0],None,[256],[0,256])
+    hist_tile4 = cv2.calcHist([tile4],[0],None,[256],[0,256])
     return np.concatenate((hist_tile1, hist_tile2, hist_tile3, hist_tile4))
+
+def get_hs_multi_hist(img):
+    ''' Paramenters: img (color image)
+        Returns: return several hist concat result of splitting the image '''
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    (h, w) = hsv.shape[:2]
+    tile1 = hsv[0:h//2, 0:w//2]
+    tile2 = hsv[0:h//2, w//2:-1]
+    tile3 = hsv[h//2:-1, 0:w//2]
+    tile4 = hsv[h//2:-1, w//2:-1]
+
+    hist_tile1 = cv2.calcHist([tile1],[0],None,[256],[0,256])
+    hist_tile2 = cv2.calcHist([tile2],[0],None,[256],[0,256])
+    hist_tile3 = cv2.calcHist([tile3],[0],None,[256],[0,256])
+    hist_tile4 = cv2.calcHist([tile4],[0],None,[256],[0,256])
+    return np.concatenate((hist_tile1, cv2.calcHist([tile1],[1],None,[256],[0,256]), \
+        hist_tile2, cv2.calcHist([tile2],[1],None,[256],[0,256]), hist_tile3, 
+        cv2.calcHist([tile3],[1],None,[256],[0,256]), hist_tile4, 
+        cv2.calcHist([tile4],[1],None,[256],[0,256])))
 
 
 def get_hs_concat_hist_st(img):
@@ -166,8 +187,10 @@ def get_descriptors(img):
     descript_dic['ycrcb_concat_hist'] = get_ycrcb_concat_hist(img)
     descript_dic['hsv_concat_hist'] = get_hsv_concat_hist(img)
     descript_dic['hs_concat_hist'] = get_hs_concat_hist(img)
-    descript_dic['hs_concat_hist_st'] = get_hs_concat_hist_st(img)
+    #descript_dic['hs_concat_hist_st'] = get_hs_concat_hist_st(img)
     descript_dic['hs_concat_hist_blur'] = get_hs_concat_hist_blur(img)
     descript_dic['hsv_concat_hist_blur'] = get_hsv_concat_hist_blur(img)
+    descript_dic['h_multi_hist'] = get_h_multi_hist(img)
+    descript_dic['hs_multi_hist'] = get_hs_multi_hist(img)
     return descript_dic
 
