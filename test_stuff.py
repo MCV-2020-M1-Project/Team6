6fe150f2e8c3025b0362e0994495a995c5526308
jchavescoelho <jchavescoelho@gmail.com>
2020-10-16 01:26:05 +0200

    Added 3 channel visualization and image shuffler, both for getting examples for slides

--- test_stuff.py
+++ test_stuff.py
@@ -3,8 +3,21 @@ import cv2
 import numpy as np
 import descriptor_lib as descs
 import distance_metrics_lib as dists
-import os
+import os, time
+import random as rnd
 
+
+def same_hist():
+    rnd_img = im1.copy()
+    rnd.shuffle(rnd_img)
+
+    a = descs.get_descriptors(im1)
+    b = descs.get_descriptors(rnd_img)
+
+    while cv2.waitKey(0) is not ord('c'):
+        cv2.imshow('img1',cv2.resize(im1, (256, 256)))
+        cv2.imshow('rnd_img',cv2.resize(rnd_img, (256, 256)))
+        dists.display_comparison(a['bgr_concat_hist'], b['bgr_concat_hist'])
 
 
 
@@ -17,9 +30,10 @@ def distances():
     a = descs.get_descriptors(im1)
     b = descs.get_descriptors(im2)
     
-    DESCR = 'hs_concat_hist'
+    DESCR = 'hsv_concat_hist'
 
-    dists.display_comparison(a[DESCR], b[DESCR])
+    while cv2.waitKey(0) is not ord('c'):
+        dists.display_comparison(a[DESCR], b[DESCR])
 
 
 def visualize_hist():
@@ -34,11 +48,12 @@ if __name__ == '__main__':
 
     BD_path = ['..', 'datasets']
 
-    BD_img = 'bbdd_00170.jpg'
-    query_img = '00003.jpg'
+    BD_img = 'bbdd_00009.jpg'
+    query_img = '00004.jpg'
 
     im1 = cv2.imread(os.path.join(*BD_path, 'BBDD', BD_img), 1)
-    im2 = cv2.imread(os.path.join(*BD_path, 'qsd2_w1', query_img), 1)
+    im2 = cv2.imread(os.path.join(*BD_path, 'qsd1_w1', query_img), 1)
 
     # distances()
-    visualize_hist()
+    # visualize_hist()
+    same_hist()
