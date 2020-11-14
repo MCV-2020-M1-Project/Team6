fixed ORB and added variance condition to -1 images

kp_descriptors.py
@@ -4,9 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pickle as pkl
-from libs import box_retrieval,background_removal,distance_metrics
+from libs import box_retrieval, background_removal, distance_metrics
 import retrieval_evaluation as re
 from skimage.feature import daisy
+import pandas as pd
 
 # img = cv2.imread('../datasets/qsd1_w4/00000.jpg')
 # if img is None:
@@ -28,14 +29,16 @@ That's the file where you can quickly evaluate picture by picture and draw them
 works along with index_db_kp_desc
 """
 
+
 def get_SIFT_desc(img, mask=None):
     # Initiate SIFT detector
     sift = cv2.SIFT_create()
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img,mask)
+    kp1, des1 = sift.detectAndCompute(img, mask)
     temp = des1
     # print(temp)
     return temp
+
 
 def get_ORB_desc(img, mask=None):
     # Initiate ORB detector
@@ -47,7 +50,8 @@ def get_ORB_desc(img, mask=None):
     temp = des1
     return temp
 
-def SIFT(img1,img2):
+
+def SIFT(img1, img2):
     # Initiate SIFT detector
     sift = cv2.SIFT_create()
     # find the keypoints and descriptors with SIFT
@@ -60,11 +64,11 @@ def SIFT(img1,img2):
     # print(type(des2[0]))
     # print('sift des2=',des2[0])
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1,des2,k=2)
+    matches = bf.knnMatch(des1, des2, k=2)
     # Apply ratio test
     good = []
-    for m,n in matches:
-        if m.distance < 0.3*n.distance:
+    for m, n in matches:
+        if m.distance < 0.3 * n.distance:
             good.append([m])
     # cv.drawMatchesKnn expects list of lists as matches.
     # # print('Num matches:',  len(good))
@@ -75,54 +79,16 @@ def SIFT(img1,img2):
     # good = [m for m in matches if m.distance < 55]
     return len(good)
 
-def ORB(img1,img2,mask=None):
-    # Initiate ORB detector
-    orb = cv2.ORB_create()
-    # find the keypoints and descriptors with ORB
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    kp2, des2 = orb.detectAndCompute(img2, mask)
-    
-    print('kp=',len(kp1))
-    # print(kp1[0].pt, kp1[0].angle)
-    good = []
-    if True: # method 1 for getting matches
-        # create BFMatcher object
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        # Match descriptors.
-        matches = bf.match(des1, des2)
-        # Sort them in the order of their distance.
-        matches = sorted(matches, key=lambda x: x.distance)
-        # Draw first 10 matches.
-        print('matches=',len(matches))
-
-        img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:20], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        plt.imshow(img3), plt.show()
-        good = [m for m in matches if m.distance < 35]
-        distance = [matches[m].distance for m in range(0,10)]
-        print("good=", len(good))
-        print("distance=",distance)
-        # cv.drawMatchesKnn expects list of lists as matches.
-        # print('Num matches:',  len(good))
-        # img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        # plt.imshow(img3),plt.show()
-    else:
-        bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des1,des2,k=2)
-        for m,n in matches:
-            if m.distance < 0.6*n.distance:
-                good.append([m])
-
-    return len(good)
-
 
 def load_index():
-    path = ['..','pkl_data','kp_bd_descriptors.pkl'] #for making the path system independent
+    path = ['..', 'pkl_data', 'kp_bd_descriptors.pkl']  # for making the path system independent
     db_descript_list = []
     with open(os.path.join(*path), 'rb') as dbfile:
         db_descript_list = pkl.load(dbfile)
     return db_descript_list
 
-def get_bf_matching(des1, des2,descriptor=None):
+
+def get_bf_matching(des1, des2, descriptor=None):
     if descriptor == 'orb2':
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         # Match descriptors.
@@ -134,6 +100,7 @@ def get_bf_matching(des1, des2,descriptor=None):
         matches = bf.knnMatch(des1, des2, k=2)
     return matches
 
+
 def get_flann_matching(des1, des2):
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
@@ -142,21 +109,22 @@ def get_flann_matching(des1, des2):
     matches = flann.knnMatch(des1, des2, k=2)
     return matches
 
-def get_best_matches(des1,descriptor, measure = 'BFM'):
+
+def get_best_matches(des1, descriptor, measure='BFM'):
     results = []
     db_list = load_index()
 
     for item in db_list:
         # print(item.keys())
-        print('idex=',item['idx'])
-        print("desc=",descriptor)
+        print('idex=', item['idx'])
+        print("desc=", descriptor)
         des2 = item[descriptor]
         if des2 is None: continue
         # print(type(des2[0]))
-        print('shape of des=',des2.shape)
+        print('shape of des=', des2.shape)
         # quit()
         if measure == 'BFM':
-            matches = get_bf_matching(des1,des2, descriptor)
+            matches = get_bf_matching(des1, des2, descriptor)
         elif measure == 'flann':
             matches = get_flann_matching(des1, des2)
         # Apply ratio test
@@ -165,39 +133,42 @@ def get_best_matches(des1,descriptor, measure = 'BFM'):
             if m.distance < 0.75 * n.distance:
                 good.append([m])
         # cv.drawMatchesKnn expects list of lists as matches.
-        print(item['idx'] ,' , matching points= ', len(good))
-        results.append((item['idx'],len(good)))
+        print(item['idx'], ' , matching points= ', len(good))
+        results.append((item['idx'], len(good)))
     return results
 
-def compare_SIFT(img1,descriptor='sift',mask=None,measure='BFM'):
+
+def compare_SIFT(img1, descriptor='sift', mask=None, measure='BFM'):
     # Initiate SIFT detector
-    des1 = get_SIFT_desc(img1,mask)
+    des1 = get_SIFT_desc(img1, mask)
     db_list = load_index()
     # print(db_list[2].values())
 
-    return get_best_matches(des1,descriptor, measure)
+    return get_best_matches(des1, descriptor, measure)
 
-def compare_ORB(img1,descriptor='orb',mask=None,measure='BFM'):
+
+def compare_ORB(img1, descriptor='orb', mask=None, measure='BFM'):
     # Initiate ORB detector
-    des1 = get_ORB_desc(img1,mask)
+    des1 = get_ORB_desc(img1, mask)
 
     db_list = load_index()
     # print(db_list[2].values())
 
-    return get_best_matches(des1,descriptor,measure)
+    return get_best_matches(des1, descriptor, measure)
+
 
 def DAISY(img1, img2):
-    img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     img1 = cv2.resize(img1, (512, 512), img1)
     img2 = cv2.resize(img2, (512, 512), img2)
 
-    des1,viz1= daisy(img1, step=180, radius=58, rings=2, histograms=6,
-                         orientations=8, visualize=True)
+    des1, viz1 = daisy(img1, step=180, radius=58, rings=2, histograms=6,
+                       orientations=8, visualize=True)
     len(des1)
 
-    des2,viz2= daisy(img2, step=180, radius=58, rings=2, histograms=6,
-                         orientations=8, visualize=True)
+    des2, viz2 = daisy(img2, step=180, radius=58, rings=2, histograms=6,
+                       orientations=8, visualize=True)
     # BFMatcher with default params
     # result = get_flann_matching(des1[0],des2[0])
 
@@ -207,54 +178,111 @@ def DAISY(img1, img2):
     vectors = min(len(des1), len(des2))
     hist1 = np.concatenate([des1[x][y] for x in range(0, vectors) for y in range(0, vectors)])
     hist2 = np.concatenate([des2[x][y] for x in range(0, vectors) for y in range(0, vectors)])
-    outcome =distance_metrics.get_l1_distance(hist1,hist2)
+    outcome = distance_metrics.get_l1_distance(hist1, hist2)
 
-    print('hist=',len(hist1))
+    print('hist=', len(hist1))
 
     result = outcome
 
+    return viz1, viz2, result
 
-    return viz1,viz2,result
+
+def ORB(img1, img2, mask=None):
+    # Initiate ORB detector
+    orb = cv2.ORB_create(scaleFactor=1.1, fastThreshold=10, WTA_K=2)
+    # find the keypoints and descriptors with ORB
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, mask)
+
+    # print('kp=', len(kp1))
+    # print(kp1[0].pt, kp1[0].angle)
+    good = []
+    if True:  # method 1 for getting matches
+        # create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        # Match descriptors.
+        matches = bf.match(des1, des2)
+        # Sort them in the order of their distance.
+        matches = sorted(matches, key=lambda x: x.distance)
+        # Draw first 10 matches.
+        # print('matches=', len(matches))
+        matches_calc = [m.distance for m in matches[:len(matches) // 10]]
+        var = np.var(np.array(matches_calc))
+        # print('var=', var)
+        # img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:20], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        # plt.imshow(img3), plt.show()
+        good = [m for m in matches if m.distance < 35]
+        # distance = ([matches[m].distance for m in range(0, 40)])
+        # print("good=", len(good))
+        # print("distance=", distance)
+        # cv.drawMatchesKnn expects list of lists as matches.
+        # print('Num matches:',  len(good))
+        # img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        # plt.imshow(img3),plt.show()
+    else:
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1, des2, k=2)
+        for m, n in matches:
+            if m.distance < 0.6 * n.distance:
+                good.append([m])
+
+    return len(good), var
 
 
 def main():
     # img1 = cv2.imread(r'../../datasets/BBDD/bbdd_00104.jpg')
     # img2 = cv2.imread(r'../../datasets/qsd1_w4/00008.jpg', cv2.IMREAD_COLOR)
-    img1 = cv2.imread(r'../datasets/BBDD/bbdd_00237.jpg')
+    img1 = cv2.imread(r'../datasets/BBDD/bbdd_00213.jpg')
     test = 0
     # img2 = cv2.imread('../datasets/qsd1_w4/{:05d}.jpg'.format(test), cv2.IMREAD_COLOR)
-    img2 = cv2.imread('../datasets/qsd1_w4/00005.jpg', cv2.IMREAD_COLOR)
+    img2 = cv2.imread('../datasets/qsd1_w5/00003.jpg', cv2.IMREAD_COLOR)
     # 250,6
     # ORB(img1,img2)
     # SIFT(img1, img2)
     # get_SIFT_desc(img2)
 
     mask_background = background_removal.method_canny_multiple_paintings(img2.copy())[1]
-
     masks = re.sort_rects_lrtb(mask_background)
-    # print(masks)
-    for mask in masks:
+    print(masks)
+    for id, mask in enumerate(masks):
         if abs(mask[1] - mask[3]) < 50:
             continue
-
         v1 = mask[:2]
         v2 = mask[2:]
         img_no_bg = img2[v1[1]:v2[1], v1[0]:v2[0]]
-    mask=masks[1]
+        if id == 0: break
+    mask = masks[0]
     v1 = mask[:2]
     v2 = mask[2:]
     img_no_bg = img2[v1[1]:v2[1], v1[0]:v2[0]]
     cv2.imshow('im', img_no_bg)
     cv2.waitKey(0)
     mask = box_retrieval.get_boxes(img_no_bg)
-    cv2.imshow('mask',mask*255)
-    cv2.waitKey()
+    # cv2.imshow('mask',mask*255)
+    # cv2.waitKey()
     # des= DAISY(img_no_bg)
-    # img1=cv2.resize(img1,(512,512),img1)
     mask = cv2.resize(mask, (512, 512), mask)
     img_no_bg = cv2.resize(img_no_bg, (512, 512), img_no_bg)
-    img_no_bg=cv2.medianBlur(img_no_bg,3)
+    img_no_bg = cv2.medianBlur(img_no_bg, 5)
+    cv2.imshow('img',img_no_bg)
+    cv2.waitKey()
+    path = ['..', 'datasets', 'BBDD']
+    qs_number = len([name for name in os.listdir(os.path.join(*path)) \
+                     if '.jpg' in name])
+    list_var = []
+    for i in range(0, qs_number):
+        path = ['..', 'datasets', 'BBDD', 'bbdd_{:05d}'.format(i) + '.jpg']
+        path = os.path.join(*path)
+        # print(path)
+        img1 = cv2.imread(path, cv2.IMREAD_COLOR)
 
+        img1 = cv2.resize(img1, (512, 512), img1)
+        m, var = ORB(img1, img_no_bg, 1 - mask)
+        list_var.append([m, var])
+    print('Done')
+    print(sorted(list_var))
+    df = pd.DataFrame(list_var)
+    df.to_csv('../output.csv')
     # print(f'testing for image {test}:')
     # for i in range(287):
     #     img1 = cv2.imread('../datasets/BBDD/bbdd_{:05d}.jpg'.format(i))
@@ -265,7 +293,7 @@ def main():
     #         print(i, m)
 
     # ORB(img1,img_no_bg)
-    print('Done')
+
     # quit()
     # viz1,viz2, result = DAISY(img1,img_no_bg)
     #
@@ -279,11 +307,12 @@ def main():
     # cv2.waitKey()
     # quit()
     # results = compare_SIFT(img_no_bg, descriptor='sift',mask= 1-mask, measure='flann')
-    m = ORB(img1, img_no_bg,1-mask)
-    results = compare_ORB(img_no_bg,descriptor='orb', mask=1-mask, measure='BFM')
 
-    results.sort(key=lambda x: x[1],reverse=True)
-    print(results[:3])
+    # results = compare_ORB(img_no_bg,descriptor='orb', mask=1-mask, measure='BFM')
+
+    # results.sort(key=lambda x: x[1],reverse=True)
+    # print(results[:3])
+
 
 main()
 

libs/descriptors.py
@@ -18,9 +18,9 @@ def painting_in_db(des1, dataset, mask=None, method=1):
 
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     # bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
-
+    biggest_match = 0
     # Iterate over the ds
-    for ds_im in dataset:
+    for id,ds_im in enumerate(dataset):
 
         des2 = ds_im['orb']
         if des2 is None:
@@ -28,8 +28,18 @@ def painting_in_db(des1, dataset, mask=None, method=1):
         # print(len(des1), len(des2))
         good = []
         matches = bf.match(des1, des2)
+        matches = sorted(matches, key=lambda x: x.distance)
+        index_compare = len(matches)//10
         good = [m for m in matches if m.distance < 35]
-        if len(good) > 4: return False
+        matches_calc = [m.distance for m in matches[:index_compare]]
+        var = np.var(np.array(matches_calc))
+        # print("id=",id,"len good = ", len(good) , "var=", var)
+        # print('var=', var)
+        # if len(good) > 4: return False
+        if len(good)>biggest_match: biggest_match=len(good)
+        if len(good) > 3:
+            if var < 20:
+                return False
 
     return True
 
@@ -130,8 +140,6 @@ def painting_in_db2(des1, dataset, mask=None, method=1):
         total_sum += len(good) if len(good) > 0 else 0
 
     return False
-
-
 
 
 def get_daisy_desc(img):